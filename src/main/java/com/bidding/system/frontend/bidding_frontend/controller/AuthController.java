b0ee4c03eb2cb3cf22d033efe9d59cef9ad1d506
Beatrice <beatricesilva1113@gmail.com>
2026-05-28 12:03:46 -0300

    design front

--- src/main/java/com/bidding/system/frontend/bidding_frontend/controller/AuthController.java
+++ src/main/java/com/bidding/system/frontend/bidding_frontend/controller/AuthController.java
@@ -71,13 +71,13 @@ public class AuthController {
    
     }
 
-    /*
+    
     @GetMapping("/logout")
     public String logout(HttpSession session) {
         session.invalidate();
         return "redirect:/login";
     }
-    */
+    
 
 }
     
